7985117e4be85b7f619aa2bca18bc03ac9b74ca1
Cory Mickelson <mickelsoncory@gmail.com>
2018-04-11 12:52:37 -0700

    remove static binary files, update export.default to export

--- index.js
+++ index.js
@@ -19,80 +19,58 @@ Object.defineProperty(exports, "__esModule", { value: true });
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 const data_1 = require("./data");
+exports.Data = data_1.Data;
 const document_1 = require("./document");
+exports.Document = document_1.Document;
+exports.FontEncoding = document_1.FontEncoding;
 const object_1 = require("./object");
+exports.Obj = object_1.Obj;
 const page_1 = require("./page");
+exports.Page = page_1.Page;
 const annotation_1 = require("./annotation");
+exports.Annotation = annotation_1.Annotation;
+exports.NPDFAction = annotation_1.NPDFAction;
+exports.NPDFAnnotation = annotation_1.NPDFAnnotation;
+exports.NPDFAnnotationFlag = annotation_1.NPDFAnnotationFlag;
+exports.NPDFAnnotationType = annotation_1.NPDFAnnotationType;
 const field_1 = require("./field");
+exports.CheckBox = field_1.CheckBox;
+exports.Field = field_1.Field;
+exports.TextField = field_1.TextField;
+exports.ListBox = field_1.ListBox;
+exports.ComboBox = field_1.ComboBox;
 const image_1 = require("./image");
+exports.Image = image_1.Image;
 const painter_1 = require("./painter");
+exports.Painter = painter_1.Painter;
+exports.Encoding = painter_1.Encoding;
+exports.ExtGState = painter_1.ExtGState;
+exports.Font = painter_1.Font;
+exports.NPDFAlignment = painter_1.NPDFAlignment;
+exports.NPDFBlendMode = painter_1.NPDFBlendMode;
+exports.NPDFColorSpace = painter_1.NPDFColorSpace;
+exports.NPDFFontType = painter_1.NPDFFontType;
+exports.NPDFLineCapStyle = painter_1.NPDFLineCapStyle;
+exports.NPDFLineJoinStyle = painter_1.NPDFLineJoinStyle;
+exports.NPDFRenderingIntent = painter_1.NPDFRenderingIntent;
+exports.NPDFStokeStyle = painter_1.NPDFStokeStyle;
+exports.NPDFTextRenderingMode = painter_1.NPDFTextRenderingMode;
+exports.NPDFVerticalAlignment = painter_1.NPDFVerticalAlignment;
 const rect_1 = require("./rect");
+exports.Rect = rect_1.Rect;
 const signer_1 = require("./signer");
+exports.Signer = signer_1.Signer;
+exports.signature = signer_1.signature;
 const stream_1 = require("./stream");
+exports.Stream = stream_1.Stream;
 const form_1 = require("./form");
+exports.Form = form_1.Form;
 const parser_1 = require("./parser");
+exports.ContentsTokenizer = parser_1.ContentsTokenizer;
 const reference_1 = require("./reference");
+exports.Ref = reference_1.Ref;
 const table_1 = require("./table");
-exports.default = {
-    ContentsTokenizer: parser_1.ContentsTokenizer,
-    Form: form_1.Form,
-    Stream: stream_1.Stream,
-    Rect: rect_1.Rect,
-    Image: image_1.Image,
-    Data: data_1.Data,
-    Document: document_1.Document,
-    FontEncoding: document_1.FontEncoding,
-    Obj: object_1.Obj,
-    Page: page_1.Page,
-    Annotation: annotation_1.Annotation,
-    NPDFAction: annotation_1.NPDFAction,
-    NPDFAnnotation: annotation_1.NPDFAnnotation,
-    NPDFAnnotationFlag: annotation_1.NPDFAnnotationFlag,
-    NPDFAnnotationType: annotation_1.NPDFAnnotationType,
-    CheckBox: field_1.CheckBox,
-    Field: field_1.Field,
-    TextField: field_1.TextField,
-    ListBox: field_1.ListBox,
-    ComboBox: field_1.ComboBox,
-    Painter: painter_1.Painter,
-    Encoding: painter_1.Encoding,
-    ExtGState: painter_1.ExtGState,
-    Font: painter_1.Font,
-    NPDFAlignment: painter_1.NPDFAlignment,
-    NPDFBlendMode: painter_1.NPDFBlendMode,
-    NPDFColorSpace: painter_1.NPDFColorSpace,
-    NPDFFontType: painter_1.NPDFFontType,
-    NPDFLineCapStyle: painter_1.NPDFLineCapStyle,
-    NPDFLineJoinStyle: painter_1.NPDFLineJoinStyle,
-    NPDFRenderingIntent: painter_1.NPDFRenderingIntent,
-    NPDFStokeStyle: painter_1.NPDFStokeStyle,
-    NPDFTextRenderingMode: painter_1.NPDFTextRenderingMode,
-    NPDFVerticalAlignment: painter_1.NPDFVerticalAlignment,
-    Signer: signer_1.Signer,
-    signature: signer_1.signature,
-    Ref: reference_1.Ref,
-    Cell: table_1.Cell,
-    Table: table_1.Table,
-};
+exports.Cell = table_1.Cell;
+exports.Table = table_1.Table;
 exports.CONVERSION = 0.0028346456693;
-// export const Document = doc,
-//     Page = page,
-//     Obj = obj,
-//     Annotation = annot,
-//     NPdfAction = action,
-//     NPdfAnnotation = annotenum,
-//     NPdfAnnotationFlag = annotflags,
-//     NPdfAnnotationType = annottype,
-//     Field = field,
-//     Form = form,
-//     CheckBox = checkbox,
-//     TextField = textfield,
-//     ListBox = listbox,
-//     ComboBox = combobox,
-//     Image = image,
-//     Painter = painter,
-//     Rect = rect,
-//     Signature = sign,
-//     Stream = stream,
-//     ContentsTokenizer = ct,
 //# sourceMappingURL=index.js.map
