01b94dc822319fdb1811878e4e28c62aa902bfec
Cory Mickelson <mickelsoncory@gmail.com>
2018-06-06 15:47:48 -0700

    package only shared libraries required for aws environment

--- data.d.ts
+++
@@ -1,7 +0,0 @@
-import { NPDFInternal } from "./object";
-export declare class Data {
-    private _instance;
-    readonly value: string;
-    constructor(value: string | NPDFInternal);
-    write(output: string): void;
-}
--- form.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var ISigFlags;
-(function (ISigFlags) {
-    ISigFlags[ISigFlags["SignatureExists"] = 1] = "SignatureExists";
-    ISigFlags[ISigFlags["AppendOnly"] = 2] = "AppendOnly";
-    ISigFlags[ISigFlags["SignatureExistsAppendOnly"] = 3] = "SignatureExistsAppendOnly";
-})(ISigFlags = exports.ISigFlags || (exports.ISigFlags = {}));
-//# sourceMappingURL=form.js.map
